ea60c4027b8e5c98d0fc801f990604a0482a04ea
Mitchel Parish <mitchelparish@gmail.com>
2019-01-29 23:50:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,15 @@
 class Triangle
-  # write code here
+
+  def initialize(side1, side2, side3)
+  end
+
+  def kind
+  end
+
+    class TriangleError < StandardError
+      def message
+        ""
+
+    end
+
 end
