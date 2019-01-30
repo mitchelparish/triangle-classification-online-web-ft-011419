635b94f315299fcb3ae72e292c85ea37f2688fc6
Mitchel Parish <mitchelparish@gmail.com>
2019-01-30 00:01:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,7 @@ class Triangle
      elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
        :isosceles
      elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3
-       :scalene 
+       :scalene
      end
    end
  end
