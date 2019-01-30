94b33af0a389dd09f746b2a9b1cef7d1738f3136
Mitchel Parish <mitchelparish@gmail.com>
2019-01-30 00:02:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,7 +5,7 @@ class Triangle
     @side_3 = side_3
   end
 
-  def kind()
+  def kind
     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
       raise TriangleError
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
@@ -19,13 +19,8 @@ class Triangle
         :scalene
       end
     end
-
   end
 
   class TriangleError < StandardError
-
   end
-
-
-
 end
